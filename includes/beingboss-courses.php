0c49fea62391737ddf4182231fa26ef2ca7f4970
coreymwinter <hello@coreymwinter.com>
2021-01-08 17:31:38 -0600

    Add Courses endpoint to WC account

--- includes/beingboss-courses.php
+++ includes/beingboss-courses.php
@@ -267,29 +267,29 @@ function cmb2_get_course_products_list() {
  *
  * @param string $output current content to display. The filter function should append to this and return
  * @param string $course_status 'not_started', 'in_progress' or 'complete'.
- * @param int $course_id ID of the Course being viewed. 
- * @param int $user_id ID of the User viewing the Course. 
- * 
+ * @param int $course_id ID of the Course being viewed.
+ * @param int $user_id ID of the User viewing the Course.
+ *
  * @return string The $output variable containing any new content.
  *
  * @since LeansDash 2.3
  */
 //add_filter('ld_after_course_status_template_container', 'add_content_after_course_status', 10, 4 );
 //function add_content_after_course_status( $output = '', $course_status = 'not_started', $course_id = 0, $user_id = 0 ) {
-    
+
 
   //  if ( $course_status == 'not_started' ) {
   //      $output .= '<p>Today is a good day to start learning.</p>';
-  //  } 
-    
+  //  }
+
    // else if ( $course_status == 'in_progress' ) {
   //      $output .= '<p>Continue the Course and continue learning.</p>';
    // }
-    
+
    // else if ( $course_status == 'complete' ) {
    //     $output .= '<p>Thank You for completing this Course.</p>';
    // }
-        
+
     //return $output;
 //}
 
@@ -420,4 +420,110 @@ function cmb2_get_lesson_forum_list() {
 
 
 
-?>
+
+
+
+class My_Custom_My_Account_Endpoint {
+
+ /**
+	* Custom endpoint name.
+	*
+	* @var string
+	*/
+ public static $endpoint = 'courses'; // HERE GOES THE ENDPOINT URL. IF YOU ADD courses THEN THE ENDPOINT URL WILL BE /my-account/courses/
+
+ /**
+	* Plugin actions.
+	*/
+ public function __construct() {
+	 // Actions used to insert a new endpoint in the WordPress.
+	 add_action( 'init', array( $this, 'add_endpoints' ) );
+	 add_filter( 'query_vars', array( $this, 'add_query_vars' ), 0 );
+
+	 // Change the My Accout page title.
+	 add_filter( 'the_title', array( $this, 'endpoint_title' ) );
+
+	 // Insering your new tab/page into the My Account page.
+	 add_filter( 'woocommerce_account_menu_items', array( $this, 'new_menu_items' ) );
+	 add_action( 'woocommerce_account_' . self::$endpoint .  '_endpoint', array( $this, 'endpoint_content' ) );
+ }
+
+ /**
+	* Register new endpoint to use inside My Account page.
+	*
+	* @see https://developer.wordpress.org/reference/functions/add_rewrite_endpoint/
+	*/
+ public function add_endpoints() {
+	 add_rewrite_endpoint( self::$endpoint, EP_ROOT | EP_PAGES );
+ }
+
+ /**
+	* Add new query var.
+	*
+	* @param array $vars
+	* @return array
+	*/
+ public function add_query_vars( $vars ) {
+	 $vars[] = self::$endpoint;
+	 return $vars;
+ }
+
+ /**
+	* Set endpoint title.
+	*
+	* @param string $title
+	* @return string
+	*/
+ public function endpoint_title( $title ) {
+	 global $wp_query;
+	 $is_endpoint = isset( $wp_query->query_vars[ self::$endpoint ] );
+
+	 if ( $is_endpoint && ! is_admin() && is_main_query() && in_the_loop() && is_account_page() ) {
+		 // New page title.
+		 $title = __( 'My Courses', 'woocommerce' );
+		 remove_filter( 'the_title', array( $this, 'endpoint_title' ) );
+	 }
+
+	 return $title;
+ }
+
+ /**
+	* Insert the new endpoint into the My Account menu.
+	*
+	* @param array $items
+	* @return array
+	*/
+ public function new_menu_items( $items ) {
+	 // Remove the logout menu item.
+	 $logout = $items['customer-logout'];
+	 unset( $items['customer-logout'] );
+
+	 // Insert your custom endpoint.
+	 $items[ self::$endpoint ] = __( 'My Courses', 'woocommerce' );
+
+	 // Insert back the logout item.
+	 $items['customer-logout'] = $logout;
+
+	 return $items;
+ }
+
+ /**
+	* Endpoint HTML content.
+	*/
+ public function endpoint_content() {
+	 echo do_shortcode('[ld_profile]'); // HERE GOES LEARNDASH SHORTCODE
+ }
+
+ /**
+	* Plugin install action.
+	* Flush rewrite rules to make our custom endpoint available.
+	*/
+ public static function install() {
+	 flush_rewrite_rules();
+ }
+}
+
+new My_Custom_My_Account_Endpoint();
+
+// Flush rewrite rules on plugin activation.
+register_activation_hook( __FILE__, array( 'My course', 'install' ) );
