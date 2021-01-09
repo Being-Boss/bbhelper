da4c2fcec76a416561b380479a13fdb71715e2cc
coreymwinter <hello@coreymwinter.com>
2021-01-08 18:38:03 -0600

    version bump

--- beingboss.php
+++ beingboss.php
@@ -3,7 +3,7 @@
 Plugin Name: Being Boss Helper
 Plugin URI:  https://www.beingboss.club
 Description: Custom Wordpress Functions for Being Boss
-Version:     1.9.2
+Version:     1.9.9
 Author:      Corey Winter
 Author URI:  https://coreymwinter.com
 License:     GPLv2
